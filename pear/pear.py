0201b8a8e62b1d8917b3aadcb0dc92c2b313338c
Mark Adel Nawar <markadelnawar@gmail.com>
2021-08-07 15:37:14 +0200

    add the thread_task function

--- pear/pear.py
+++ pear/pear.py
@@ -1,12 +1,24 @@
 from logging import error
 from .pear_thread import PearThread
+import threading
 
 class Pear():
+    global lock
+    lock = threading.Lock()
+
     def __init__(self):
         self.threads = set([])
     
     def __check_race_conditions__(self):
         return False
+
+    def thread_task(self,args):
+        if self.get_thread_names().count(args[1].__name__) == 1 :
+            args[1](*(args[2]))
+        else:
+            args[0].acquire()
+            args[1](*(args[2]))
+            args[0].release() 
 
     def get_thread_by_id(self, id):
         for thread in self.threads:
@@ -26,8 +38,9 @@ class Pear():
         return [thread.get_id() for thread in self.threads]
 
     def add_thread(self, func, args):
-        t = PearThread(func, args)
-        t.set_name(func.__name__ + '(' + str(args) + ')')
+        fun_arg = (lock , func , args)
+        t = PearThread(self.thread_task, fun_arg)
+        t.set_name(func.__name__)
         self.threads.add(t)
 
     def remove_thread(self, id):
