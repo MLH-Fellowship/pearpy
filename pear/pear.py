dc3ac8814540412919643da4a0b6a2fd9128f4c3
Aiden Szeto <aszeto35@gmail.com>
2021-08-11 09:41:52 -0700

    Updated readme and added comments

--- pear/pear.py
+++ pear/pear.py
@@ -8,16 +8,20 @@ class Pear():
 
     def __init__(self):
         self.threads = set([])
-    
-    def __check_race_conditions__(self):
-        return False
-    
+
     def thread_task(self , args):
+        # Lock resources if threads utilize the same function
         if self.get_thread_names().count(args[1].__name__) == 1:
-            args[1](*(args[2]))
+            if type(args[2]) is list:
+                args[1](*(args[2]))
+            else:
+                args[1](args[2])
         else:
             args[0][args[1].__name__].acquire()
-            args[1](*(args[2]))
+            if type(args[2]) is list:
+                args[1](*(args[2]))
+            else:
+                args[1](args[2])
             args[0][args[1].__name__].release()
 
     def get_thread_by_id(self, id):
@@ -39,27 +43,29 @@ class Pear():
 
     def add_thread(self, func, args):
         fun_arg = (self.locks , func , args)
-
+        # Create lock if thread uses duplicate function
         if not func.__name__ in self.locks.keys():
             self.locks[func.__name__] = Lock()
-        
+        # Add thread to thread pool and set name to function name
         t = PearThread(self.thread_task, fun_arg)
         t.set_name(func.__name__)
         self.threads.add(t)
 
     def remove_thread(self, id):
-        for thread in self.threads:
-            if id is thread.get_id():
-                self.threads.remove(self.get_thread_by_id(id))
-                return
+        # Remove thread corresponding to id from thread pool
+        if id in self.get_thread_ids():
+            self.threads.remove(self.get_thread_by_id(id))
+        # Throw error if id does not correspond to any thread
+        else:
+            raise ValueError('ID does not correspond to any threads')
 
     def run(self):
-        try:
-            for thread in self.threads:
-                thread.start()
-            for thread in self.threads:
-                thread.join()
-            return True
-        except error:
-            print(error)
+        # Throw error and return false if thread pool is empty
+        if len(self.threads) == 0:
+            raise ValueError('Cannot run Pear with no threads')
             return False
+        for thread in self.threads:
+            thread.start()
+        for thread in self.threads:
+            thread.join()
+        return True
